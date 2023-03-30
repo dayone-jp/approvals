5ad4d8c3a54bf6c359c654e9efc2b88bbdb81e9d
aleksei-commmune <ivanov.aleksei@commmune.jp>
2023-03-30 14:59:39 +0900

    first try

---
+++ index.js
@@ -0,0 +1,19 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
+try {
+  const prNumber = github.context.payload.pull_request.number;
+  const reviews = await github
+    .getOctokit(env.GITHUB_TOKEN)
+    .request('GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
+      ...github.context.repo,
+      pull_number: prNumber,
+    });
+
+  core.setOutput(
+    'approvals',
+    reviews.data.filter((r) => r.state === 'APPROVED'),
+  );
+} catch (error) {
+  core.setFailed(error.message);
+}
